3727ab4e95f1a77fffb1d33cddab428d4d61922e
Daniel Cleveland <dpcleveland@gmail.com>
2016-05-17 17:10:12 -0500

    Featured projects slider

---
+++ scripts/custom.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+    loop:false,
+    nav:true,
+    margin:20,
+    dots:true,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+});
+});
